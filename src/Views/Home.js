771811db9cfae3eba72f6a381dc484cf1e28ce92
jotapixma <jorgeteran22@gmail.com>
2019-12-31 11:37:01 -0300

    any changes

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../assets/styles/views/Home.scss';
 // import ProductCard from '../components/ProductCard.jsx'
-// import ProductCardDetail from '../components/ProductCardDetail.jsx'
-// import CategorySection from '../components/CategorySection.jsx'
-// import PurchaseBar from '../components/PurchaseBar.jsx'
+import ProductCardDetail from '../components/ProductCardDetail.jsx'
+import CategorySection from '../components/CategorySection.jsx'
+import PurchaseBar from '../components/PurchaseBar.jsx'
 // import ProductFeature from '../components/ProductFeature.jsx'
 import SliderFeature from '../components/SliderFeature'
 
@@ -11,13 +11,13 @@ import SliderFeature from '../components/SliderFeature'
 function Home() {
   return (
     <div className="home">
-      <SliderFeature/>
+      {/* <SliderFeature/> */}
       {/* <ProductFeature/> */}
       {/* <SmallProductCard/> */}
-      {/* <CategorySection/> */}
-      {/* <ProductCardDetail/> */}
-      {/* <ProductCardDetail/> */}
-      {/* <PurchaseBar/> */}
+      <CategorySection/>
+      <ProductCardDetail/>
+      <ProductCardDetail/>
+      <PurchaseBar/>
       {/* <ProductCard/> */}
       {/* <ProductCard/>  */}
     </div>
