3d4d535b9f5a1ad45876f8fd3afc4a310ea544f8
jotapixma <jorge.teran@cuponatic.com>
2020-01-10 13:44:37 -0300

    fix resources

--- src/components/SliderFeature.jsx
+++ src/components/SliderFeature.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 // import Slider from "react-slick";
 import { AutoRotatingCarousel } from 'material-auto-rotating-carousel';
 import '../assets/styles/components/SliderFeature.scss';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import SmallProductCard from '../components/SmallProductCard.jsx';
 
 // const Slide = require('./Slide').default;
